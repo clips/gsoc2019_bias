299193601316121b36d37ee5cfc79475a6ca7da2
PanosIs <panoslantavos@gmail.com>
2019-06-09 18:15:12 +0300

    Expand on database implementation

--- gatherers/twitter/data/database_api.py
+++ gatherers/twitter/data/database_api.py
@@ -1,8 +1,9 @@
 from mongoengine import connect
 
 from gatherers.twitter.data.database_odm import Tweets
+from gatherers.twitter.utils.singleton import Singleton
 
-class Mongo_API:
+class Mongo_API(metaclass=Singleton):
     def __init__(self):
         db_name = 'Bias'
         connect(db_name)
@@ -12,7 +13,9 @@ class Mongo_API:
         tweet.save()
 
     def insert_parse_for_tweet(self, tweet_id : int, tweet_body : str, user_name : str = None):
-        pass
+        Tweets.objects(id = tweet_id).update_one(set__tweet_body = tweet_body)
+        if(user_name is not None):
+            Tweets.objects(id=tweet_id).update_one(set__user_name=user_name)
 
     def get_tweets(self, dataset_name : str = None, parsed : bool = True, limit : int = 100):
         if dataset_name is None:
--- gatherers/twitter/data/database_odm.py
+++ gatherers/twitter/data/database_odm.py
@@ -2,11 +2,14 @@ from mongoengine import *
 
 class Tweets(Document):
     id = LongField(primary_key=True)
-    user_name = StringField(required=False)
-    tweet_content = StringField(required=False)
+
     parsed = BooleanField(required=True, default=False)
     dataset_name = StringField(required=True)
     label = StringField(required=True)
+    parseable = BooleanField(required=True, default=False)
+
+    user_name = StringField(required=False)
+    tweet_body = StringField(required=False)
 
     meta = {
         'indexes': [

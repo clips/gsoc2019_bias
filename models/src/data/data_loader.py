8f8905a55771e00b7ab15935f98f0f738d51b56a
PanosIs <panoslantavos@gmail.com>
2019-06-09 17:08:14 +0300

    More project structure and mock classes and files

---
+++ models/src/data/data_loader.py
@@ -0,0 +1,14 @@
+from torch.utils.data import Dataset
+
+class Token_Dataset(Dataset):
+    def __init__(self):
+        pass
+
+class Embedding_Dataset(Dataset):
+    pass
+
+def get_token_dataset():
+    pass
+
+def get_embedding_dataset():
+    pass
---
+++ models/src/utils/tensor_utils.py
@@ -0,0 +1,2 @@
+def convert_sparse_matrix_to_sparse_tensor():
+    pass

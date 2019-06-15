8ad8f6f353a1df3f2cea67125280d87a3cbdfb98
PanosIs <panoslantavos@gmail.com>
2019-06-15 22:39:21 +0300

    Minor refactoring, add min_df as class parameter

--- models/src/data/token_data_loader.py
+++ models/src/data/token_data_loader.py
@@ -5,13 +5,13 @@ import pandas as pd
 import numpy as np
 
 class Token_Dataset(Dataset):
-    def __init__(self, frame : pd.DataFrame, sparse = True):
+    def __init__(self, frame : pd.DataFrame, min_df = 5, sparse = True):
         self.texts = frame["texts"]
         self.labels = frame["labels"].astype('category')
         self.num_labels = self.labels.cat.codes
 
 
-        self.tokenizer = TfidfVectorizer(strip_accents='unicode', stop_words='english', max_df=0.97, min_df=0.03)
+        self.tokenizer = TfidfVectorizer(strip_accents='unicode', stop_words='english', min_df=min_df)
         self.token_texts = self.tokenizer.fit_transform(self.texts)
         if not sparse: self.token_texts = self.token_texts.toarray()
 
@@ -19,10 +19,11 @@ class Token_Dataset(Dataset):
         return len(self.labels)
 
     def __getitem__(self, index):
-        return self.token_texts[index], self.labels[index]
+        return self.token_texts[index], self.num_labels[index]
 
 def get_token_dataset():
     pass
 
 if __name__ == "__main__":
-    ds = Token_Dataset(pd.read_csv("hatespeech-data.csv", sep='\t', header=None, names=["texts", "labels"], usecols=(0,1)))
+    ds = Token_Dataset(pd.read_csv("hatespeech-data.csv", sep='\t', header=None, names=["texts", "labels"], usecols=(0,1)),sparse=False)
+    print(ds[4])

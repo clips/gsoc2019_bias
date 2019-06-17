fa45c7556219ea835cc35f5fa533bef94141ca24
PanosIs <panoslantavos@gmail.com>
2019-06-17 15:32:58 +0300

    Add plotting functions

---
+++ models/src/utils/plotter.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
+from seaborn import heatmap
+import numpy as np
+
+def plot_loss_graph(training_loss, test_loss):
+    plt.plot(training_loss, label = "Training")
+    plt.plot(test_loss, label = "Test")
+    plt.ylabel('Loss')
+    plt.xlabel('Epochs')
+    plt.legend(loc='upper left')
+    plt.show()
+
+def plot_confusion_matrix(matrix):
+    labels = [0,1,2]
+    matrix = np.int32(matrix)
+    fig, ax = plt.subplots(figsize=(15, 10))
+    heatmap(matrix, annot=True, fmt = "d",
+                xticklabels=labels, yticklabels=labels)
+    plt.ylabel('Actual')
+    plt.xlabel('Predicted')
+    plt.show()

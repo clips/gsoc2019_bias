ed6601c89c839c372276d9b7cba97f14cd47ad7f
PanosIs <panoslantavos@gmail.com>
2019-07-03 18:55:42 +0300

    Implement base PersistentDataset class

--- models/src/data/persistent_dataset.py
+++ models/src/data/persistent_dataset.py
@@ -1,9 +1,12 @@
 from abc import ABC, abstractmethod
-from torch.utils.data import Dataset
 
-class PersistentDataset(ABC, Dataset):
+import pandas
+from sklearn.model_selection import train_test_split
+
+#Abstract class for a dataset that supports saving and loading data from files
+class PersistentDataset(ABC):
     @abstractmethod
-    def load_data(self, filename):
+    def load_data(self, dataframe, test_percentile):
         pass
 
     @abstractmethod
@@ -11,5 +14,31 @@ class PersistentDataset(ABC, Dataset):
         pass
 
     @abstractmethod
-    def load_train_test(self, train_filename, test_filename):
+    def load_train_test(self, train_filename, test_filename, preprocessed = True):
         pass
+
+#Base implementation of the persistent dataset, can produce train test splits to be used by both the other dataset classes
+class PersistentDatasetImpl(PersistentDataset):
+    def __init__(self):
+        self.data, self.train, self.test = None, None, None
+
+    def load_data(self, dataframe, test_percentile):
+        self.data = dataframe
+        self.train, self.test = train_test_split(dataframe, test_size=test_percentile, shuffle=False)
+
+    def save_train_test(self, train_filename, test_filename):
+        if self.train is not None and self.test is not None:
+            self.train.to_csv(path_or_buf=train_filename + '.csv', sep='\t', header=False, index=False)
+            self.test.to_csv(path_or_buf=test_filename + '.csv', sep='\t', header=False, index=False)
+        else:
+            raise ValueError()
+
+    def load_train_test(self, train_filename, test_filename, preprocessed = True):
+        self.train = pandas.read_csv(train_filename + '.csv', sep='\t', header=None, names=["texts", "labels"], usecols=(0, 1))
+        self.test = pandas.read_csv(test_filename + '.csv', sep='\t', header=None, names=["texts", "labels"], usecols=(0, 1))
+
+    def get_train_test(self):
+        if self.train is not None and self.test is not None:
+            return self.train, self.test
+        else:
+            raise ValueError()

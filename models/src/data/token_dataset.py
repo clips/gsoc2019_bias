94b686cbad214ae360bc38ee7ab5bbb48c3361d1
PanosIs <panoslantavos@gmail.com>
2019-07-03 18:56:07 +0300

    Expand TokenDataset to implement PersistentDataset functions

---
+++ models/src/data/token_dataset.py
@@ -0,0 +1,62 @@
+import torch
+from torch.utils.data import Dataset, TensorDataset
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+import pandas
+
+from models.src.data.persistent_dataset import PersistentDataset
+from models.src.utils.sparse_utils import save_sparse_csr, load_sparse_csr, convert_sparse_matrix_to_tensor
+
+class TokenDataset(PersistentDataset):
+    def __init__(self):
+        self.data, self.train_tokens, self.test_tokens = None, None, None
+        self.train_labels, self.test_labels = None, None
+
+    def load_data(self, dataframe, test_percentile):
+        self.data = dataframe
+        #TODO: Establish if TFIDF vectorizer maintains the pandas index
+        train, test = train_test_split(dataframe, test_size=test_percentile, shuffle=False)
+        self._process_data(train, test)
+
+    def save_train_test(self, train_filename, test_filename):
+        if self.train_tokens is not None and self.test_tokens is not None:
+            save_sparse_csr(train_filename, self.train_tokens)
+            save_sparse_csr(test_filename, self.test_tokens)
+            self.train_labels.to_csv(path_or_buf=train_filename + '.csv',sep='\t', header=False, index=False)
+            self.test_labels.to_csv(path_or_buf=test_filename + '.csv',sep='\t', header=False, index=False)
+        else:
+            raise ValueError()
+
+    def load_train_test(self, train_filename, test_filename, preprocessed = True):
+        if preprocessed is True:
+            self.train_tokens = load_sparse_csr(train_filename)
+            self.test_tokens = load_sparse_csr(test_filename)
+            self.train_labels = pandas.read_csv(train_filename + '.csv', sep='\t', header=None, names=["labels"])
+            self.test_labels = pandas.read_csv(test_filename + '.csv', sep='\t', header=None, names=["labels"])
+        else:
+            train = pandas.read_csv(train_filename + '.csv', sep='\t', header=None, names=["texts", "labels"], usecols=(0, 1))
+            test = pandas.read_csv(test_filename + '.csv', sep='\t', header=None, names=["texts", "labels"], usecols=(0, 1))
+            self._process_data(train, test)
+
+    def get_train_dataset(self) -> Dataset:
+        input_tensor = convert_sparse_matrix_to_tensor(self.train_tokens)
+        label_tensor = torch.LongTensor(self.train_labels)
+        return TensorDataset(input_tensor, label_tensor)
+
+    def get_test_dataset(self) -> Dataset:
+        input_tensor = convert_sparse_matrix_to_tensor(self.train_tokens)
+        label_tensor = torch.LongTensor(self.train_labels)
+        return TensorDataset(input_tensor, label_tensor)
+
+    def _process_data(self, train, test):
+        train_texts, train_labels = train['texts'], train['labels'].astype('category')
+        test_texts, test_labels = test['texts'], test['labels'].astype('category')
+
+        self.train_labels = train_labels.cat.codes
+        self.test_labels = test_labels.cat.codes
+
+        tokenizer = TfidfVectorizer(strip_accents='unicode', stop_words='english', min_df=10)
+        tokenizer.fit(train['texts'])
+
+        self.train_tokens = tokenizer.transform(train['texts'])
+        self.test_tokens = tokenizer.transform(test['texts'])

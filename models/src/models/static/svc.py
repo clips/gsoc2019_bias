7136af8cb4c32485f4d5381cbab402aa149c5240
PanosIs <panoslantavos@gmail.com>
2019-07-03 15:52:56 +0300

    Convert SVC to use unified classifier parent class

--- models/src/models/static/svc.py
+++ models/src/models/static/svc.py
@@ -1,29 +1,42 @@
 import time
-import numpy as np
 from sklearn.svm import LinearSVC
-from torch.utils.data import random_split
-
+import joblib
 
 from models.src.data.token_data_loader import get_token_dataset
+from models.src.models.model import classifier
 
-class SVM_Classifier:
+class SVM_Wrapper(classifier):
     def __init__(self, C=1, loss='hinge', penalty='l2', max_iter=10000):
         self.classifier = LinearSVC(C=C, loss=loss, penalty=penalty, max_iter=max_iter)
+        self.training_time = None
 
     def train(self, x_train, y_train):
         start = time.perf_counter()
         self.classifier.fit(x_train, y_train)
-        self.time = time.perf_counter() - start
+        self.training_time = time.perf_counter() - start
 
-    def score(self, x_test, y_test):
-        return self.classifier.score(x_test, y_test)
+    def predict(self, x_test):
+        return self.classifier.predict(x_test)
 
-    def predict(self, x):
-        return self.classifier.predict(x)
+    def predict_one(self, x_single):
+        return self.classifier.predict(x_single)
+
+    def save_model(self, filename: str):
+        joblib.dump(self.classifier, filename)
+
+    def load_model(self, filename: str):
+        self.classifier = joblib.load(filename)
+
+    def get_training_time(self):
+        if self.training_time is None:
+            raise ValueError()
+        else:
+            return self.training_time
+
 
 if __name__ == "__main__":
     train_set, test_set = get_token_dataset()
 
-    svm = SVM_Classifier()
+    svm = SVM_Wrapper()
     svm.train(train_set.token_texts, train_set.num_labels)
-    print(svm.score(test_set.token_texts, test_set.num_labels))
+    svm.save_model("SVMModel.pk")

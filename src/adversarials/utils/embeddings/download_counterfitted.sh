cea1546e38140a2448cf374b71a6d7ec5661b8ce
PanosIs <panoslantavos@gmail.com>
2019-07-07 19:32:14 +0300

    Add download scripts

---
+++ src/adversarials/utils/embeddings/download_counterfitted.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+wget https://raw.githubusercontent.com/nmrksic/counter-fitting/master/word_vectors/counter-fitted-vectors.txt.zip
+unzip counter-fitted-vectors.txt.zip
+rm counter-fitted-vectors.txt.zip
---
+++ src/adversarials/utils/embeddings/download_glove.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+wget http://nlp.stanford.edu/data/glove.840B.300d.zip
+unzip glove.840B.300d.zip
+rm glove.840B.300d.zip

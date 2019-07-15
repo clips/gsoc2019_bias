85ef681d38fc1908d6159b14f56954a88e641070
PanosIs <panoslantavos@gmail.com>
2019-07-15 16:07:10 +0300

    Minor code formatting

--- adversarials/src/utils/embeddings/build_embeddings.py
+++ adversarials/src/utils/embeddings/build_embeddings.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 import pickle
 
@@ -16,14 +15,14 @@ if not os.path.exists('aux_files'):
 imdb_dataset = data_utils.IMDBDataset(path=IMDB_PATH, max_vocab_size=MAX_VOCAB_SIZE)
 
 # save the dataset
-with open(('aux_files/dataset_%d.pkl' %(MAX_VOCAB_SIZE)), 'wb') as f:
+with open(('aux_files/dataset_%d.pkl' % MAX_VOCAB_SIZE), 'wb') as f:
     pickle.dump(imdb_dataset, f)
 
 # create the glove embeddings matrix (used by the classification model)
 glove_model = glove_utils.load_glove_model(GLOVE_PATH)
 glove_embeddings, _ = glove_utils.create_embeddings_matrix(glove_model, imdb_dataset.dict, imdb_dataset.full_dict)
 # save the glove_embeddings matrix
-np.save('aux_files/embeddings_glove_%d.npy' %(MAX_VOCAB_SIZE), glove_embeddings)
+np.save('aux_files/embeddings_glove_%d.npy' % MAX_VOCAB_SIZE, glove_embeddings)
 
 # Load the counterfitted-vectors (used by our attack)
 glove2 = glove_utils.load_glove_model('counter-fitted-vectors.txt')
@@ -31,6 +30,6 @@ glove2 = glove_utils.load_glove_model('counter-fitted-vectors.txt')
 counter_embeddings, missed = glove_utils.create_embeddings_matrix(glove2, imdb_dataset.dict, imdb_dataset.full_dict)
 
 # save the embeddings for both words we have found, and words that we missed.
-np.save(('aux_files/embeddings_counter_%d.npy' %(MAX_VOCAB_SIZE)), counter_embeddings)
-np.save(('aux_files/missed_embeddings_counter_%d.npy' %(MAX_VOCAB_SIZE)), missed)
+np.save(('aux_files/embeddings_counter_%d.npy' % MAX_VOCAB_SIZE), counter_embeddings)
+np.save(('aux_files/missed_embeddings_counter_%d.npy' % MAX_VOCAB_SIZE), missed)
 print('All done')

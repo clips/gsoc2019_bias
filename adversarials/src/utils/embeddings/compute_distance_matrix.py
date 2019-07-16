4513c23a99c5ec817e55b1f5c7a7e983517d46cc
PanosIs <panoslantavos@gmail.com>
2019-07-16 19:20:21 +0300

    Externalize matrix dtype to environment variable

--- adversarials/src/utils/embeddings/compute_distance_matrix.py
+++ adversarials/src/utils/embeddings/compute_distance_matrix.py
@@ -1,17 +1,21 @@
 import numpy as np
-import pickle
+import os
 
 from src.utils.embeddings import glove_utils
 
+DTYPE = os.getenv('MAT_DTYPE', 'float16')
+
 class DistanceMatrix:
     def compute_distance_matrix(self, embedding_matrix):
-        c_ = -2 * np.dot(embedding_matrix.T, embedding_matrix)
-        a = np.sum(np.square(embedding_matrix), axis=0).reshape((1, -1))
-        self.dist = a + a.T + c_
+        c_ = -2 * np.dot(embedding_matrix.T, embedding_matrix).astype(dtype=DTYPE)
+        a = np.sum(np.square(embedding_matrix), axis=0).reshape((1, -1)).astype(dtype=DTYPE)
+        self.dist = (a + a.T + c_).astype(dtype=DTYPE)
+        print(self.dist)
 
-    def test_distance_matrix(self, word, dict, inv_dict):
+    def test_distance_matrix(self, word, dict, inv_dict, add_vector = None):
+        print(dict[word])
         src_word = dict[word]
         neighbours, neighbours_dist = glove_utils.pick_most_similar_words(src_word, self.dist)
-        print('Closest words to `good` are :')
+        print('Closest words to {} are :'.format(word))
         result_words = [inv_dict[x] for x in neighbours]
         print(result_words)

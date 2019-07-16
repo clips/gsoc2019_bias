14cd55c97f128f6ecfb20310cdcb9a1e31f4c202
PanosIs <panoslantavos@gmail.com>
2019-07-16 19:21:04 +0300

    Implement cosine distance metric and test for gendered word translation

--- adversarials/src/utils/test/embeddings_test.py
+++ adversarials/src/utils/test/embeddings_test.py
@@ -1,24 +1,45 @@
 import logging
+import os
+
+import numpy
+import scipy.spatial.distance as dist
+import csv
 
 from src.utils.data.data_utils import TwitterDataset
 from src.utils.embeddings.compute_distance_matrix import DistanceMatrix
 from src.utils.embeddings.glove_utils import *
 
 TWITTER_PATH = "hatespeech-data.csv"
-GLOVE_PATH = "glove.twitter.27B.100d.txt"
+GLOVE_PATH = os.getenv('GLOVE_PATH').replace("\"", "").split(sep=',')
+GENDER_PATH = "gendered-word-pairs.csv"
 
-LOG_FORMAT = ('%(levelname) -5s %(asctime) -10s %(name) -5s %(funcName) -5s %(lineno) -10d: %(message)s')
-LOGGER = logging.getLogger(__name__)
-logging.basicConfig(level=logging.DEBUG, format=LOG_FORMAT)
+def get_closest_words(matrix, dict, add, minus, limit, threshold = None):
+    vector = numpy.zeros(matrix.shape[0])
+    for word in add:
+        vector += matrix[:,dict[word]]
+    for word in minus:
+        vector -= matrix[:,dict[word]]
+
+    words_with_distance = [(1 - dist.cosine(vector, matrix[:,dict[w]]), w) for w in dict.keys()]
+    return sorted(words_with_distance, key=lambda t: t[0], reverse=True)[:10]
 
 if __name__ == "__main__":
     print("Loading dataset dictionary")
     dict, inv_dict = TwitterDataset(path=TWITTER_PATH, min_instances=5).get_dict()
-    print("Loading glove model")
-    glove_model = load_glove_model(GLOVE_PATH)
-    print("Creating embedding matrix")
-    matrix, not_found = create_embeddings_matrix(glove_model, dict, d=100)
-    dist_matrix = DistanceMatrix()
-    print("Computing distance matrix")
-    dist_matrix.compute_distance_matrix(matrix)
-    dist_matrix.test_distance_matrix("good", dict, inv_dict)
+    for glove_name in GLOVE_PATH:
+        print("Loading glove model, name = {}".format(glove_name))
+        glove_model, embedding_dim = load_glove_model(glove_name)
+
+        print("Creating embedding matrix, dim = {}".format(embedding_dim))
+        matrix, not_found = create_embeddings_matrix(glove_model, dict, d=embedding_dim)
+        print("Words not found in embedding file: {}".format(len(not_found)))
+
+        with open(GENDER_PATH, 'r') as f:
+            reader = csv.reader(f)
+            word_list = list(reader)
+
+        for row in word_list:
+            print("Pair: {}:{}".format(row[0], row[1]) )
+            if row[0] in dict.keys() and row[1] in dict.keys():
+                print("Male to female: " + get_closest_words(matrix, dict, [row[0], 'woman'], ['man'], 10).__str__())
+                print("Female to male: " + get_closest_words(matrix, dict, [row[1], 'man'], ['woman'], 10).__str__())

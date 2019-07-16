d68ceca8b9c301e4b76955d40cfc651528eb2ebc
PanosIs <panoslantavos@gmail.com>
2019-07-16 16:00:11 +0300

    Implement embedding loader class for greater versatility

--- adversarials/src/utils/embeddings/load_embeddings.py
+++ adversarials/src/utils/embeddings/load_embeddings.py
@@ -6,30 +6,48 @@ import os
 from src.utils.data import data_utils
 from src.utils.embeddings import glove_utils
 
-IMDB_PATH = 'aclImdb'
-MAX_VOCAB_SIZE = 50000
-GLOVE_PATH = 'glove.840B.300d.txt'
 
-if not os.path.exists('aux_files'):
-    os.mkdir('aux_files')
-imdb_dataset = data_utils.IMDBDataset(path=IMDB_PATH, max_vocab_size=MAX_VOCAB_SIZE)
+class EmbeddingLoader:
+    def __init__(self, embedding_path, dataset_path):
+        self.path = embedding_path
+        self.dict = self._load_twitter_dataset(dataset_path)
 
-# save the dataset
-with open(('aux_files/dataset_%d.pkl' % MAX_VOCAB_SIZE), 'wb') as f:
-    pickle.dump(imdb_dataset, f)
+    def _load_twitter_dataset(self, dataset_path):
+        set = data_utils.TwitterDataset(path=dataset_path)
+        return set.dict
 
-# create the glove embeddings matrix (used by the classification model)
-glove_model = glove_utils.load_glove_model(GLOVE_PATH)
-glove_embeddings, _ = glove_utils.create_embeddings_matrix(glove_model, imdb_dataset.dict, imdb_dataset.full_dict)
-# save the glove_embeddings matrix
-np.save('aux_files/embeddings_glove_%d.npy' % MAX_VOCAB_SIZE, glove_embeddings)
+    def _load_embeddings(self):
+        model = glove_utils.load_glove_model
+        self.matrix = glove_utils.create_embeddings_matrix(model, self.dict)
 
-# Load the counterfitted-vectors (used by our attack)
-glove2 = glove_utils.load_glove_model('counter-fitted-vectors.txt')
-# create embeddings matrix for our vocabulary
-counter_embeddings, missed = glove_utils.create_embeddings_matrix(glove2, imdb_dataset.dict, imdb_dataset.full_dict)
+    def get_embedding_matrix(self):
+        return self.matrix
 
-# save the embeddings for both words we have found, and words that we missed.
-np.save(('aux_files/embeddings_counter_%d.npy' % MAX_VOCAB_SIZE), counter_embeddings)
-np.save(('aux_files/missed_embeddings_counter_%d.npy' % MAX_VOCAB_SIZE), missed)
-print('All done')
+
+if __name__ == "__main__":
+    IMDB_PATH = 'aclImdb'
+    MAX_VOCAB_SIZE = 50000
+    GLOVE_PATH = 'glove.840B.300d.txt'
+    if not os.path.exists('aux_files'):
+        os.mkdir('aux_files')
+    imdb_dataset = data_utils.IMDBDataset(path=IMDB_PATH, max_vocab_size=MAX_VOCAB_SIZE)
+
+    # save the dataset
+    with open(('aux_files/dataset_%d.pkl' % MAX_VOCAB_SIZE), 'wb') as f:
+        pickle.dump(imdb_dataset, f)
+
+    # create the glove embeddings matrix (used by the classification model)
+    glove_model = glove_utils.load_glove_model(GLOVE_PATH)
+    glove_embeddings, _ = glove_utils.create_embeddings_matrix(glove_model, imdb_dataset.dict, imdb_dataset.full_dict)
+    # save the glove_embeddings matrix
+    np.save('aux_files/embeddings_glove_%d.npy' % MAX_VOCAB_SIZE, glove_embeddings)
+
+    # Load the counterfitted-vectors (used by our attack)
+    glove2 = glove_utils.load_glove_model('counter-fitted-vectors.txt')
+    # create embeddings matrix for our vocabulary
+    counter_embeddings, missed = glove_utils.create_embeddings_matrix(glove2, imdb_dataset.dict, imdb_dataset.full_dict)
+
+    # save the embeddings for both words we have found, and words that we missed.
+    np.save(('aux_files/embeddings_counter_%d.npy' % MAX_VOCAB_SIZE), counter_embeddings)
+    np.save(('aux_files/missed_embeddings_counter_%d.npy' % MAX_VOCAB_SIZE), missed)
+    print('All done')

0ef8a820131e21c27ecc904001372b600a6e814a
PanosIs <panoslantavos@gmail.com>
2019-07-16 19:41:46 +0300

    Improve logging to track results

--- adversarials/src/utils/test/embeddings_test.py
+++ adversarials/src/utils/test/embeddings_test.py
@@ -2,6 +2,7 @@ import logging
 import os
 
 import numpy
+import pandas
 import scipy.spatial.distance as dist
 import csv
 
@@ -23,6 +24,14 @@ def get_closest_words(matrix, dict, add, minus, limit, threshold = None):
     words_with_distance = [(1 - dist.cosine(vector, matrix[:,dict[w]]), w) for w in dict.keys()]
     return sorted(words_with_distance, key=lambda t: t[0], reverse=True)[:10]
 
+def get_similarity(matrix, dict, add, minus, target):
+    vector = numpy.zeros(matrix.shape[0])
+    for word in add:
+        vector += matrix[:,dict[word]]
+    for word in minus:
+        vector -= matrix[:,dict[word]]
+    return 1 - dist.cosine(vector, matrix[:,dict[target]])
+
 if __name__ == "__main__":
     print("Loading dataset dictionary")
     dict, inv_dict = TwitterDataset(path=TWITTER_PATH, min_instances=5).get_dict()
@@ -38,8 +47,20 @@ if __name__ == "__main__":
             reader = csv.reader(f)
             word_list = list(reader)
 
+        top3 = 0
         for row in word_list:
             print("Pair: {}:{}".format(row[0], row[1]) )
             if row[0] in dict.keys() and row[1] in dict.keys():
-                print("Male to female: " + get_closest_words(matrix, dict, [row[0], 'woman'], ['man'], 10).__str__())
-                print("Female to male: " + get_closest_words(matrix, dict, [row[1], 'man'], ['woman'], 10).__str__())
+                closest_m2f = get_closest_words(matrix, dict, [row[0], 'woman'], ['man'], 10)
+                closest_f2m = get_closest_words(matrix, dict, [row[1], 'man'], ['woman'], 10)
+                similarity_m2f = get_similarity(matrix, dict, [row[0], 'woman'], ['man'], row[1])
+                similarity_f2m = get_similarity(matrix, dict, [row[1], 'man'], ['woman'], row[0])
+
+                if(row[1] in [i[1] for i in closest_m2f[:3]]):
+                    top3 += 1
+                if(row[0] in [i[1] for i in closest_f2m[:3]]):
+                    top3 += 1
+
+                print("\tMale to female, Similarity: {} \n\t Words: ".format(similarity_m2f) + get_closest_words(matrix, dict, [row[0], 'woman'], ['man'], 10).__str__())
+                print("\tFemale to male, Similarity: {} \n\t Words: ".format(similarity_f2m) + get_closest_words(matrix, dict, [row[1], 'man'], ['woman'], 10).__str__())
+        print("Top3 results: {}".format(top3))

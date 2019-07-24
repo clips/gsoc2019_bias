b2c26a2d499a6d419865e3792e38702fc97b63ea
PanosIs <panoslantavos@gmail.com>
2019-07-24 16:35:04 +0300

    Delete .dockerignore

--- adversarials/scripts/docker_test_embeddings.sh
+++ adversarials/scripts/docker_test_embeddings.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 echo 'y' | docker image prune
 docker build -f docker/test_embeddings/Dockerfile --tag test_embeddings .
-docker run --rm -it -v resources:/app test_embeddings /bin/bash
+docker run --rm -it -v /resources:/app/resources test_embeddings /bin/bash

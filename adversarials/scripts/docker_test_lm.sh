a583be4a839ef99e32bb3b05a4e4f9b4b0a3766f
PanosIs <panoslantavos@gmail.com>
2019-07-24 16:59:29 +0300

    Finally properly mount volumes (Hopefully)

--- adversarials/scripts/docker_test_lm.sh
+++ adversarials/scripts/docker_test_lm.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 echo 'y' | docker image prune
 docker build -f docker/test_lm/Dockerfile --tag test_language_model .
-docker run --rm -it -v resources:/app test_language_model /bin/bash
+docker run --rm -it -v "$(pwd)/resources":/app/resources test_language_model /bin/bash

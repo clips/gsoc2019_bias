f8aa075911cfafbda10544cb772a60873eb5eb77
PanosIs <panoslantavos@gmail.com>
2019-07-30 16:44:45 +0300

    Initial pipeline test commit

---
+++ adversarials/scripts/docker_test_attack.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+echo 'y' | docker image prune
+docker build -f docker/test_attack/Dockerfile --tag test_attack .
+docker run --rm -it --runtime=nvidia -v "$(pwd)/resources":/app/resources test_attack /bin/bash

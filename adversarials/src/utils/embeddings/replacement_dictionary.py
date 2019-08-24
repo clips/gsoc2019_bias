eaf2bbee2be592dcbe62ef5d6d47e5b56456b09b
PanosIs <panoslantavos@gmail.com>
2019-08-24 04:34:23 +0300

    Revert change, since it incurred lower accuracy

--- adversarials/src/utils/embeddings/replacement_dictionary.py
+++ adversarials/src/utils/embeddings/replacement_dictionary.py
@@ -7,7 +7,7 @@ from src.utils.embeddings.glove_utils import get_closest_words
 from sklearn.feature_extraction import stop_words
 
 class ReplacementDictionary:
-    def __init__(self, matrix, word_idx, add = None, minus = None, vocabulary = None, limit = 4, dynamic = True):
+    def __init__(self, matrix, word_idx, add = None, minus = None, vocabulary = None, limit = 3, dynamic = True):
         if minus is None:
             minus = []
         self.minus = minus
@@ -36,10 +36,12 @@ class ReplacementDictionary:
                 self.replacements[word] = []
             else:
                 try:
+                    standard_replacements = [item[1] for item in
+                                             get_closest_words(self.matrix, self.word_idx, [word], [], 4)]
                     swap_replacements = [item[1] for item in
-                                         get_closest_words(self.matrix, self.word_idx, self.add + [word], self.minus, self.limit)]
+                                         get_closest_words(self.matrix, self.word_idx, self.add + [word], self.minus, 2)]
 
-                    self.replacements[word] = [item for item in swap_replacements if item != word]
+                    self.replacements[word] = [item for item in swap_replacements if item not in standard_replacements]
                 except Exception as e:
                     self.replacements[word] = []
         return self.replacements[word]
